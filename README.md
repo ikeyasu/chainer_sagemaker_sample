54033054ed1d20113be36f7d587f326d4ab47b57
ikeyasu <ikeyasu@gmail.com>
2019-09-06 09:56:53 +0900

    Add sample of SageMaker local mode.

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # chainer_sagemaker_sample
+
+## Getting started
+
+### 1. login to Docker
+
+First of all, you need to login docker with AWS ECR account.
+
+```bash
+$ $(aws ecr get-login --no-include-email --region ap-northeast-1 --registry-ids 520713654638)
+```
+
+See
+https://docs.aws.amazon.com/ja_jp/AmazonECR/latest/userguide/ECR_AWSCLI.html
+for details.
+
+
+`520713654638` is public ECR of SageMaker containers.
+
+### 2. Get IAM role and dataset
+
+1. Open SageMaker and Jupyter notebook
+2. Upload `create_data_on_sagemaker.ipynb` to the notebook and open it.
+3. Run all
+4. Note IAM role. It is like `arn:aws:iam::111111111111:role/service-role/AmazonSageMaker-ExecutionRole-20181212T121212`
+5. Note s3 path of dataset like `s3://sagemaker-ap-northeast-1-111111111111/notebook/chainer_cifar/train` and `s3://sagemaker-ap-northeast-1-111111111111/notebook/chainer_cifar/test`.
+
+### 3. Local run
+
+Make directory for SageMaker local.
+
+```bash
+$ mkdir -p ~/robo/container
+```
+
+Download `train_m.npz` and `test_m.npz`. Please use the noted s3 paths above.
+
+```bash
+$ aws s3 cp `s3://sagemaker-ap-northeast-1-111111111111/notebook/chainer_cifar/train/train_m.npz` train.npz
+$ aws s3 cp `s3://sagemaker-ap-northeast-1-111111111111/notebook/chainer_cifar/test/test_m.npz` test.npz
+```
+
+Run SageMaker local mode. Please use the IAM role above.
+
+```bash
+$ python start_train.py --local-mode --profile ated --arn arn:aws:iam::111111111111:role/service-role/AmazonSageMaker-ExecutionRole-20181212T121212
+```
---
+++ start_train.py
@@ -0,0 +1,52 @@
+import argparse
+import os
+
+import sagemaker
+from boto3.session import Session
+from sagemaker import LocalSession
+from sagemaker.chainer.estimator import Chainer
+
+
+# See https://docs.aws.amazon.com/ja_jp/sagemaker/latest/dg/pre-built-containers-frameworks-deep-learning.html
+IMAGE = '520713654638.dkr.ecr.ap-northeast-1.amazonaws.com/sagemaker-chainer:5.0.0-gpu-py3'
+
+DEFAULT_INSTANCE_TYPE = 'ml.m4.4xlarge'
+DEFAULT_REGION = 'ap-northeast-1'
+DEFAULT_RUNTIME = 30  # seconds
+
+
+def main(parser=argparse.ArgumentParser()):
+    import logging
+    logging.basicConfig(level=logging.WARN)
+
+    parser.add_argument('--profile', type=str, default='default')
+    parser.add_argument('--local-mode', action='store_true')
+    parser.add_argument('--instance-type', type=str, default=DEFAULT_INSTANCE_TYPE)
+    parser.add_argument('--region', type=str, default=DEFAULT_REGION)
+    parser.add_argument('--arn', type=str, default=None)
+    parser.add_argument('--max-runtime', type=int, default=DEFAULT_RUNTIME, help='seconds')
+    args = parser.parse_args()
+
+    boto_session = Session(profile_name=args.profile, region_name=args.region)
+    sagemaker_session = LocalSession(boto_session) if args.local_mode else Session(boto_session)
+    role = args.arn if args.arn is not None else sagemaker.get_execution_role(sagemaker_session)
+
+    gpu = 0 if args.instance_type.startswith('ml.p') and not args.local_mode else -1
+    hyperparameters = {'gpu': gpu,
+                       }
+    chainer_estimator = Chainer(entry_point='train.py',
+                                source_dir='./',
+                                role=role,
+                                image_name=IMAGE,
+                                framework_version='5.0.0',
+                                sagemaker_session=sagemaker_session,
+                                train_instance_count=1,
+                                train_instance_type='local' if args.local_mode else args.instance_type,
+                                hyperparameters=hyperparameters,
+                                base_job_name='chainer-sagemaker-sample',
+                                train_max_run=args.max_runtime)
+    chainer_estimator.fit(wait=args.local_mode)
+
+
+if __name__ == '__main__':
+    main()
